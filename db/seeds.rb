cb766505454ebcb46dd7b7a591c1c2cba3b87fb9
Excellentchoice <31359134+Excellentchoice@users.noreply.github.com>
2017-10-28 16:05:14 -0500

    Intergrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ puts "10 blog post created"
 
 5.times do |skill|
   Skill.create!(
-    title: "Rail #{skill}",
+    title: "Rails #{skill}",
     percent_utilized: 15
   )
 end
@@ -18,7 +18,7 @@ puts "5 skills created"
 
 9.times do |portfolio_item|
   Portfolio.create!(
-    title:"Portfolio title #{portfolio_item}",
+    title:'Portfolio title: #{portfolio_item}',
     subtitle:"My great service",
     body:"Ideals are a dime a dozen",
     main_image:"http://via.placeholder.com/600x400",
